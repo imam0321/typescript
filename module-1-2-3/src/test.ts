9287f3bad62792a2468d353fd3a17c3c39615b92
imam0321 <imam.hossain0321@gmail.com>
2025-05-04 19:42:05 +0600

    typescript project setup

---
+++ module-1-2-3/src/test.ts
@@ -0,0 +1,13 @@
+type Person = {
+  name: string;
+  address: string;
+  job: string
+}
+
+const user : Person = {
+  name: "imam",
+  address: "Mirpur",
+  job: "developer"
+}
+
+console.log(user);
---
+++ redme.md
@@ -0,0 +1,11 @@
+ts config install
+
+```
+tsc --init
+
+```
+npm init -y
+npm i ts-node
+
+package.json --> "dev": "ts-node-dev --respawn --transpile-only qna.ts"
+
